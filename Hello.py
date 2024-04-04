55ccd1a8a99a1b7abbd953c699c7cd52c5b17d1a
Gustavo Almeida de Jesus <66255218+SystemGuuh@users.noreply.github.com>
2024-04-04 13:54:43 +0000

    try fixes pkg-config problem

--- Hello.py
+++ Hello.py
@@ -1,10 +1,24 @@
 import streamlit as st
 from streamlit.logger import get_logger
+import subprocess
 
 LOGGER = get_logger(__name__)
 
+def installPkgConfig():
+    # Define the command to install pkg-config
+    command = "sudo apt install pkg-config"
+
+    # Execute the command
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("pkg-config installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error installing pkg-config:", e)
+
 #datatest-apps
 def run():
+    installPkgConfig()
+    
     st.set_page_config(
         page_title="Eshows Data",
         page_icon="🎤",
--- pages/0_Database.py
+++ pages/0_Database.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 from utils.queries import *
 
 def connect():
